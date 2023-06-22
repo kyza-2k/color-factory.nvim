b257774ac2923aea1f5174e0eb82bfc09a162468
Evan Smith <evan.leigh@outlook.com>
2023-06-22 12:49:11 -0400

    format: stylua

--- lua/color-factory.lua
+++ lua/color-factory.lua
@@ -1,4 +1,4 @@
-local color_utils = require("color_factory.color")
+local color_utils = require('color_factory.color')
 
 local convert = color_utils.convert
 local transform = color_utils.transform
@@ -18,14 +18,14 @@ local color_aliases = {
 --- Table defining the base colors.
 --- @type table
 local colors = {
-	purple = "#4f4f93",
+  purple = '#4f4f93',
 }
 
 --- A mapping table of color modification keys to their corresponding utility functions
 local color_modification_functions = {
-	s = transform.saturate,
-	l = transform.lighten,
-	b = transform.blend,
+  s = transform.saturate,
+  l = transform.lighten,
+  b = transform.blend,
 }
 
 --- Applies color modifications to a color
@@ -33,75 +33,71 @@ local color_modification_functions = {
 --- @param modifications table: A table of modifications to apply to the color.
 --- @return string: The modified color.
 local function modify(color, modifications)
-	for _, modification in ipairs(modifications) do
-		local key = modification[1]
-		local value = modification[2]
-		local modification_function = color_modification_functions[key]
+  for _, modification in ipairs(modifications) do
+    local key = modification[1]
+    local value = modification[2]
+    local modification_function = color_modification_functions[key]
 
-		if modification_function then
-			if key == "b" then
-				local hl, amount = modification[3], modification[4]
-				color = modification_function(color, value, hl, amount)
-			else
-				color = modification_function(color, value)
-			end
-		end
-	end
+    if modification_function then
+      if key == 'b' then
+        local hl, amount = modification[3], modification[4]
+        color = modification_function(color, value, hl, amount)
+      else
+        color = modification_function(color, value)
+      end
+    end
+  end
 
-	return color
+  return color
 end
 
 --- Creates a color instance with the given name.
 --- @param name function: The name of the color.
 --- @return table: The color instance.
 local function color(name)
-	local color_instance = { name = name, modifications = {} }
+  local color_instance = { name = name, modifications = {} }
 
-	local color_metatable = {
-		__index = color_instance,
+  local color_metatable = {
+    __index = color_instance,
 
-		--- Returns the hexadecimal representation of the color instance.
-		--- @return string: The hexadecimal color value.
-		__tostring = function(self)
-			return self:hex()
-		end,
-	}
+    --- Returns the hexadecimal representation of the color instance.
+    --- @return string: The hexadecimal color value.
+    __tostring = function(self) return self:hex() end,
+  }
 
-	--- Returns the hexadecimal representation of the color instance.
-	--- @return string: The hexadecimal color value.
-	function color_instance:hex()
-		local modified_color = modify(colors[self.name], self.modifications)
-		return modified_color
-	end
+  --- Returns the hexadecimal representation of the color instance.
+  --- @return string: The hexadecimal color value.
+  function color_instance:hex()
+    local modified_color = modify(colors[self.name], self.modifications)
+    return modified_color
+  end
 
-	--- Returns the RGB representation of the color instance.
-	--- @return number, number, number: The red, green, and blue values respectively.
-	function color_instance:rgb()
-		local modified_color = modify(colors[self.name], self.modifications)
-		return convert.hexToRGB(modified_color)
-	end
+  --- Returns the RGB representation of the color instance.
+  --- @return number, number, number: The red, green, and blue values respectively.
+  function color_instance:rgb()
+    local modified_color = modify(colors[self.name], self.modifications)
+    return convert.hexToRGB(modified_color)
+  end
 
-	for i = 1, #color_aliases, 2 do
-		local key, alias_key = color_aliases[i], color_aliases[i + 1]
+  for i = 1, #color_aliases, 2 do
+    local key, alias_key = color_aliases[i], color_aliases[i + 1]
 
-		--- Modifies the color instance with the specified value for the given key.
-		--- @param value any: The value to set for the key.
-		--- @return table: The modified color instance.
-		color_instance[key] = function(self, value, ...)
-			table.insert(self.modifications, { key, value, unpack({ ... }) })
-			return self
-		end
+    --- Modifies the color instance with the specified value for the given key.
+    --- @param value any: The value to set for the key.
+    --- @return table: The modified color instance.
+    color_instance[key] = function(self, value, ...)
+      table.insert(self.modifications, { key, value, unpack({ ... }) })
+      return self
+    end
 
-		color_instance[alias_key] = color_instance[key]
-	end
+    color_instance[alias_key] = color_instance[key]
+  end
 
-	return setmetatable(color_instance, color_metatable)
+  return setmetatable(color_instance, color_metatable)
 end
 
 return {
-	color = setmetatable({}, {
-		__index = function(_, key)
-			return color(key)
-		end,
-	}),
+  color = setmetatable({}, {
+    __index = function(_, key) return color(key) end,
+  }),
 }
--- lua/color_factory/utils.lua
+++ lua/color_factory/utils.lua
@@ -1,23 +1,23 @@
 local M = {}
 
 local attrs = {
-	fg = true,
-	bg = true,
-	sp = true,
-	blend = true,
-	bold = true,
-	italic = true,
-	standout = true,
-	underline = true,
-	undercurl = true,
-	underdouble = true,
-	underdotted = true,
-	underdashed = true,
-	strikethrough = true,
-	reverse = true,
-	nocombine = true,
-	link = true,
-	default = true,
+  fg = true,
+  bg = true,
+  sp = true,
+  blend = true,
+  bold = true,
+  italic = true,
+  standout = true,
+  underline = true,
+  undercurl = true,
+  underdouble = true,
+  underdotted = true,
+  underdashed = true,
+  strikethrough = true,
+  reverse = true,
+  nocombine = true,
+  link = true,
+  default = true,
 }
 
 -- Credit to: github.com/akinsho
@@ -26,9 +26,9 @@ local attrs = {
 local api, notify, fmt, augroup = vim.api, vim.notify, string.format, vim.api.nvim_create_augroup
 
 local err_warn = vim.schedule_wrap(function(group, attribute)
-	notify(fmt("failed to get highlight %s for attribute %s\n%s", group, attribute, debug.traceback()), "ERROR", {
-		title = fmt("Highlight - get(%s)", group),
-	}) -- stylua: ignore
+  notify(fmt('failed to get highlight %s for attribute %s\n%s', group, attribute, debug.traceback()), 'ERROR', {
+    title = fmt('Highlight - get(%s)', group),
+  }) -- stylua: ignore
 end)
 
 ---@private
@@ -36,12 +36,12 @@ end)
 ---@param ns integer?
 ---@return HLData
 local function get_hl_as_hex(opts, ns)
-	ns, opts = ns or 0, opts or {}
-	opts.link = opts.link ~= nil and opts.link or false
-	local hl = api.nvim_get_hl(ns, opts)
-	hl.fg = hl.fg and ("#%06x"):format(hl.fg)
-	hl.bg = hl.bg and ("#%06x"):format(hl.bg)
-	return hl
+  ns, opts = ns or 0, opts or {}
+  opts.link = opts.link ~= nil and opts.link or false
+  local hl = api.nvim_get_hl(ns, opts)
+  hl.fg = hl.fg and ('#%06x'):format(hl.fg)
+  hl.bg = hl.bg and ('#%06x'):format(hl.bg)
+  return hl
 end
 
 ---Get the value a highlight group whilst handling errors, fallbacks as well as returning a gui value
@@ -53,31 +53,25 @@ end
 ---@return string
 ---@overload fun(group: string): HLData
 M.get = function(group, attribute, fallback)
-	assert(group, "cannot get a highlight without specifying a group name")
-	local data = get_hl_as_hex({ name = group })
-	if not attribute then
-		return data
-	end
+  assert(group, 'cannot get a highlight without specifying a group name')
+  local data = get_hl_as_hex({ name = group })
+  if not attribute then return data end
 
-	assert(attrs[attribute], ("the attribute passed in is invalid: %s"):format(attribute))
-	local color = data[attribute] or fallback
-	if not color then
-		if vim.v.vim_did_enter == 0 then
-			api.nvim_create_autocmd("User", {
-				pattern = "LazyDone",
-				once = true,
-				callback = function()
-					err_warn(group, attribute)
-				end,
-			})
-		else
-			vim.schedule(function()
-				err_warn(group, attribute)
-			end)
-		end
-		return "NONE"
-	end
-	return color
+  assert(attrs[attribute], ('the attribute passed in is invalid: %s'):format(attribute))
+  local color = data[attribute] or fallback
+  if not color then
+    if vim.v.vim_did_enter == 0 then
+      api.nvim_create_autocmd('User', {
+        pattern = 'LazyDone',
+        once = true,
+        callback = function() err_warn(group, attribute) end,
+      })
+    else
+      vim.schedule(function() err_warn(group, attribute) end)
+    end
+    return 'NONE'
+  end
+  return color
 end
 
 return M
